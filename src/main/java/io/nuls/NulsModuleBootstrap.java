4158d3d40a5c7e69a1db2c55ac014591b35cfc81
MIMIEYES <pierreluohao@gmail.com>
2019-07-26 15:21:51 +0800

    add sdk-provider offline program

--- src/main/java/io/nuls/NulsModuleBootstrap.java
+++ src/main/java/io/nuls/NulsModuleBootstrap.java
@@ -7,6 +7,8 @@ import io.nuls.core.core.annotation.Autowired;
 import io.nuls.core.core.annotation.Service;
 import io.nuls.core.core.annotation.Value;
 import io.nuls.core.core.config.ConfigurationLoader;
+import io.nuls.core.core.ioc.SpringLiteContext;
+import io.nuls.core.model.StringUtils;
 import io.nuls.core.rpc.info.HostInfo;
 import io.nuls.core.rpc.model.ModuleE;
 import io.nuls.core.rpc.modulebootstrap.Module;
@@ -14,7 +16,6 @@ import io.nuls.core.rpc.modulebootstrap.NulsRpcModuleBootstrap;
 import io.nuls.core.rpc.modulebootstrap.RpcModule;
 import io.nuls.core.rpc.modulebootstrap.RpcModuleState;
 import io.nuls.v2.NulsSDKBootStrap;
-import io.nuls.v2.SDKContext;
 
 import java.util.Map;
 
@@ -35,24 +36,42 @@ public abstract class NulsModuleBootstrap extends RpcModule {
     MyModule myModule;
 
     public static void main(String[] args) {
+        boolean isOffline = false;
         if (args == null || args.length == 0) {
             args = new String[]{"ws://" + HostInfo.getLocalIP() + ":7771"};
+            //args = new String[]{"ws://192.168.1.40:7771"};
+        } else {
+            String arg1 = args[0];
+            if(StringUtils.isNotBlank(arg1)) {
+                arg1 = arg1.trim().toLowerCase();
+            }
+            if ("offline".equals(arg1)) {
+                isOffline = true;
+            }
         }
+        String basePackage = "io.nuls";
         ConfigurationLoader configurationLoader = new ConfigurationLoader();
         configurationLoader.load();
-        Provider.ProviderType providerType = Provider.ProviderType.valueOf(configurationLoader.getValue("providerType"));
         int defaultChainId = Integer.parseInt(configurationLoader.getValue("chainId"));
+        Provider.ProviderType providerType = Provider.ProviderType.valueOf(configurationLoader.getValue("providerType"));
         ServiceManager.init(defaultChainId, providerType);
-        NulsRpcModuleBootstrap.run("io.nuls", args);
-
-        initRpcServer(configurationLoader);
+        Map<String, ConfigurationLoader.ConfigItem> configItemMap = configurationLoader.getConfigData().get(SDK_PROVIDER_DOMAIN);
+        ConfigurationLoader.ConfigItem offline = configItemMap.get("offline");
+        if (offline != null) {
+            isOffline = Boolean.parseBoolean(offline.getValue());
+        }
+        if (!isOffline) {
+            NulsRpcModuleBootstrap.run("io.nuls", args);
+        } else {
+            SpringLiteContext.init(basePackage);
+        }
+        initRpcServer(configItemMap);
         NulsSDKBootStrap.init(defaultChainId, null);
     }
 
-    private static void initRpcServer(ConfigurationLoader configurationLoader) {
+    private static void initRpcServer(Map<String, ConfigurationLoader.ConfigItem> configItemMap) {
         String server_ip = "0.0.0.0";
         int server_port = 9898;
-        Map<String, ConfigurationLoader.ConfigItem> configItemMap = configurationLoader.getConfigData().get(SDK_PROVIDER_DOMAIN);
         if (configItemMap != null) {
             ConfigurationLoader.ConfigItem serverIp = configItemMap.get("server_ip");
             if (serverIp != null) {
